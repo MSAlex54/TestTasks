2f6402bf8c4a699d31acaebf69fd2cda8720fbc6
Alex <msalex54@gmail.com>
2020-07-23 20:52:32 +0300

    Code

--- src/Group.java
+++ src/Group.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
-public class Group {
+public class Group implements Serializable {
+    private ArrayList<String[]> list = new ArrayList<>();
+
+    public Group(String [] arr) {
+        list.add(arr);
+    }
+
+    public void add(String[] arr) {
+        list.add(arr);
+    }
+
+    public int size() {
+        return list.size();
+    }
+    public String toString(int num){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Группа " + num + "\n" );
+        for (String [] s: list) {
+            stringBuilder.append(Arrays.toString(s) + "\n");
+        }
+        return stringBuilder.toString();
+    }
+    public static final Comparator<Group> COMPARE_BY_COUNT = new Comparator<Group>() {
+        @Override
+        public int compare(Group lhs, Group rhs) {
+            return rhs.size() - lhs.size();
+        }
+    };
+
+
 }
