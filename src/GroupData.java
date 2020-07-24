35f8e06220613d93b9d34ecb30d62264bb7adc0c
Alex <msalex54@gmail.com>
2020-07-24 13:41:30 +0300

    Исправлено регулярное выражение, добавлена условие валидация пустых строк.

--- src/GroupData.java
+++ src/GroupData.java
@@ -5,9 +5,12 @@ import java.util.stream.Collectors;
 
 public class GroupData {
     public static void main(String[] args) {
-        String filePath = "C:\\1\\lng.csv"; //путь к файлу
+        Date startData = new Date();
+        String filePath = "C:\\1\\lng-big.csv"; //путь к файлу
+//        String filePath = "C:\\1\\lng.csv"; //путь к файлу
         Set<String> stringSet = new HashSet<>(); //сет для сбора уникальныйх значений
-        String regex = "((?:\\\"[^\\D]*\\\")(?:;*)){3}"; //регулярка для валидации корректности строки
+//        String regex = "((?:\\\"[^\\D]*\\\")(?:;*)){3}"; //регулярка для валидации корректности строки
+        String regex = "((?<=^|;)((((?:\\\")[\\d\\.]*(?:\\\"))|(?:))(?:;|$))){3}"; //регулярка для валидации корректности строки
         List<Group> groups = new ArrayList<>(); //лист для сбора групп
         Map<String, int[]> coordinates = new HashMap<>(); // номера групп в зависимости от положения значения String - значение, int[] - массив номеров групп для 1,2,3 столбцов
 
@@ -30,7 +33,7 @@ public class GroupData {
                 boolean needNewGroup = true; // флаг для создания новой группы
                 int groupNum = -1; // номер группы куда пойдут значения
                 for (int i = 0; i < currVals.length; i++) { //для каждого значения в строке
-                    if (!currVals[i].equals("\"\"") && coordinates.containsKey(currVals[i])) { //проверяем наличие в мапе с "координатами"
+                    if (!currVals[i].equals("\"\"") && !currVals[i].equals("") && coordinates.containsKey(currVals[i])) { //проверяем наличие в мапе с "координатами"
                         groupNum = coordinates.get(currVals[i])[i]; //если есть - достаем номер где уже есть это значение в этом столбце
                         if (groupNum != -1) { //если номер группы -1 - то значит ранее не добавлялось по данному столбцу пропускаем и проверяем дальше
                             groups.get(groupNum).add(currVals); //добавляем строку значений в группу
@@ -45,7 +48,7 @@ public class GroupData {
                     groups.add(group); //добавляем в список групп
                 }
                 for (int i = 0; i < currVals.length; i++) { //обновляем координаты
-                    if (!currVals[i].equals("\"\"")) { // если пустое значение - пропускаем
+                    if (!currVals[i].equals("\"\"")&&!currVals[i].equals("")) { // если пустое значение - пропускаем
                         int[] coordinate = coordinates.containsKey(currVals[i]) ? coordinates.get(currVals[i]) : new int[]{-1, -1, -1}; //получаем массив координат или создаем новый
                         coordinate[i] = groupNum; //присваиваем соответствующему столбцу номер групп где соддержится значение в данном столбце
                         coordinates.put(currVals[i], coordinate); // обновляем данные в мапе
@@ -59,5 +62,8 @@ public class GroupData {
         for (int i = 0; i < groups.size() ; i++) {
             System.out.println(groups.get(i).toString(i+1));
         }
+
+        Date finishDate = new Date();
+        System.out.println("Время выполнения: " + (finishDate.getTime() - startData.getTime()) / 1000 + (" сек."));
     }
 }
